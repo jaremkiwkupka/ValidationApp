b710e270db5db7380faf934433a5fd84f75f47d9
Julia <julka.jaremkiw@gmail.com>
2021-08-13 13:36:24 +0200

    Create client and server side data validation

--- api/routes/validateUserName.js
+++ api/routes/validateUserName.js
@@ -2,7 +2,13 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function(req, res, next) {
-    res.send("validate user name");
+    let regex = new RegExp('^[a-zA-Z]{2}\\d{2}[a-zA-Z]{2}$');
+
+    if (regex.test(userName)) {
+        res.send(console.log("ok"));
+    } else {
+        res.send(console.log("wrong"));
+    }
 });
 
 module.exports = router;
--- validation-app/src/components/Input/Input.js
+++ validation-app/src/components/Input/Input.js
@@ -1,14 +1,44 @@
+import { useState } from "react";
 import "./Input.scss";
 
 export const Input = () => {
+
+    const [userName, setUserName] = useState("");
+
+const handleChange = (e) => {
+      setUserName(e.target.value);
+}
+
+ const handleValidation = (e) => {
+       e.preventDefault();
+
+        let regex = new RegExp('^[a-zA-Z]{2}\\d{2}[a-zA-Z]{2}$');
+
+        if (regex.test(userName)) {
+            console.log("ok");
+            fetch('http://localhost:3000/validateUserName', {
+                method: 'GET',
+            })
+            .then(response => response.json())
+            .then(userName => {
+                console.log('Success:', userName);
+            })
+            .catch(error => {
+                console.error('Error:', error)
+            });
+        } else {
+            console.log("wrong");
+        }
+    }
+
     return (
         <section className="validation-container">
             <h1 className="validation-heading">User name</h1>
-            <div className="validation">
-                <input type="text" name="name" className="validation-input" />
+            <form className="validation" onSubmit={handleValidation}>
+                <input type="text" name="name" className="validation-input" value={userName} onChange={handleChange} />
                 <button className="validation-btn">Validate</button>
                 {/* <div className="validation-error-message">Insert correct user name</div> */}
-            </div>
+            </form>
         </section>
     )
 }
